86cdaa6743548a7d77524b0d504efd7ffcadc71b
Joungmin <jmko09@hist.co.kr>
2019-02-13 13:28:58 +0900

    check it is the valid time(one hour) in JWT refresh token when a refresh request is received

--- src/main/java/jwt/hello/JwtRefreshKeys.java
+++ src/main/java/jwt/hello/JwtRefreshKeys.java
@@ -1,6 +1,8 @@
 package jwt.hello;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -25,13 +27,30 @@ public class JwtRefreshKeys {
 		
 		for(int i = 0; jwtRefreshKeys.size() > i; i++) {
 			curJwtRefreshKey = jwtRefreshKeys.get(i);
-			logger.info("### ### ### JwtRefreshKeys - isJwtRefreshKeyUsed: {} - {} - {}", curJwtRefreshKey.getJwtRefreshKey(), curJwtRefreshKey.getUsername(), curJwtRefreshKey.getUseYn());
-			if(refreshKey.equals(curJwtRefreshKey.getJwtRefreshKey()) && "N".equals(curJwtRefreshKey.getUseYn()) && username.equals(curJwtRefreshKey.getUsername())) {
-				curJwtRefreshKey.setUseYn("Y");
-				return true;
+			
+			// compare JWT refresh token issue date to current date
+			if(addedHourToDatetime(curJwtRefreshKey.getCreateDate(), 1).compareTo(new Date()) > 0) {
+				// in one hour
+				logger.info("### ### ### JwtRefreshKeys - isJwtRefreshKeyUsed: {} - {} - {}", curJwtRefreshKey.getJwtRefreshKey(), curJwtRefreshKey.getUsername(), curJwtRefreshKey.getUseYn());
+				if(refreshKey.equals(curJwtRefreshKey.getJwtRefreshKey()) && "N".equals(curJwtRefreshKey.getUseYn()) && username.equals(curJwtRefreshKey.getUsername())) {
+					curJwtRefreshKey.setUseYn("Y");
+					return true;
+				}
+				
+			} else {
+				// one hour out
+				logger.info("### ### ### JwtRefreshKeys - isJwtRefreshKeyAvailable: refresh time is passed");
 			}
+			
 		}
 		return keyAvailBool;
+	}
+	
+	public Date addedHourToDatetime(Date inputDate, int hours) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(inputDate);
+		cal.add(Calendar.HOUR, hours);
+		return cal.getTime();
 	}
 	
 	public void addJwtRefreshKey(String refreshKey, String username) {
@@ -39,6 +58,7 @@ public class JwtRefreshKeys {
 		jwtRefreshKey.setJwtRefreshKey(refreshKey);
 		jwtRefreshKey.setUseYn("N");
 		jwtRefreshKey.setUsername(username);
+		jwtRefreshKey.setCreateDate(new Date());
 		jwtRefreshKeys.add(jwtRefreshKey);
 	}
 
@@ -58,6 +78,7 @@ public class JwtRefreshKeys {
 		private String jwtRefreshKey;
 		private String username;
 		private String useYn;
+		private Date createDate;
 	}
 
 }
